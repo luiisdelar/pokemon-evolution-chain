3746e7b7aafe59874a208573b6124a9d49cfc49c
Luis Eduardo Ortega De La Rosa <luisortegadlr@gmail.com>
2021-09-15 20:58:20 -0400

    some changues

--- pokeapp/management/commands/evolution-chains.py
+++ pokeapp/management/commands/evolution-chains.py
@@ -1,13 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 import requests
-from pokeapp.models import Pokemon
+from pokeapp.models import Pokemon, Stats, EvolutionChain
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
         evolution_chain = requests.get(f'https://pokeapi.co/api/v2/evolution-chain/{options["id"]}/').json()
         
-        self.get_evolution_chain(evolution_chain['chain'])
+        self.create_evolution_chain(evolution_chain['chain'])
         
         
     def add_arguments(self, parser):
@@ -24,17 +24,33 @@ class Command(BaseCommand):
         for item in specie['varieties']:
             pokemon = requests.get(item['pokemon']['url']).json()
             print(f' se tranforma en {pokemon["name"]}')
+
             
-            #Pokemon.objects.create()
+            # stats = Stats.objects.create(
+            #     hp = pokemon['stats'][0]['base_stat'],
+            #     attack = pokemon['stats'][1]['base_stat'],
+            #     defense = pokemon['stats'][2]['base_stat'],
+            #     special_attack = pokemon['stats'][3]['base_stat'],
+            #     special_defense = pokemon['stats'][4]['base_stat'],
+            #     speed = pokemon['stats'][5]['base_stat'],
+            # )
+
+            # obj_pokemon = Pokemon.objects.create(
+            #     iid = pokemon["id"],
+            #     name = pokemon["name"],
+            #     height = pokemon["height"],
+            #     weight = pokemon["weight"],
+            #     stats = stats
+            # )
             
-            return pokemon
+            # return obj_pokemon
             break
 
-    def get_evolution_chain(self, chain, pokemon = None):
+    def create_evolution_chain(self, chain, pokemon = None):
         print(chain['species']['name'], end='')
         
         if chain['evolves_to']:
             for item in chain['evolves_to']:
                 pokemon = self.get_pokemon(item['species']['url'])            
                 if item['evolves_to']:
-                    self.get_evolution_chain(item, pokemon)
+                    self.create_evolution_chain(item, pokemon)
